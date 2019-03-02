42a6b407904246e8a8668fd5e90d1f206ad340ff
wangxx <995107408@qq.com>
2019-03-02 12:07:10 +0800

    创建项目配置好less antd主题等

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	handleDele(index) {
+		// this.props.handleDele(index)
+	}
+	render() {
+		return (
+			<React.Fragment>
+                <div>header</div>
+			</React.Fragment>
+		)
+	}
+}
+
+export default Header;
